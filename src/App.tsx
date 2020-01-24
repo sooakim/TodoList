56972868d7b58fc0025d918df0d590c243d17818
SooaKim <alice2863@naver.com>
2020-01-24 21:59:55 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import Styled from 'styled-components/native';
+
+import { TodoListContextProvider } from '~/Context/TodoListContext';
+
+import Todo from './Screens/Todo';
+
+const Container = Styled.View`
+  flex: 1;
+  background-color: #EEE;
+`;
+
+const App = () => {
+  return (
+    <TodoListContextProvider>
+      <Container>
+        <Todo />
+      </Container>
+    </TodoListContextProvider>
+  );
+}
+export default App;
